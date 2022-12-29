ae4f560ba9965d57909fb22ae154f0dd174ce7fb
Thanapat <koop0408@gmail.com>
2022-12-29 20:13:25 +0700

    add _count2

--- lib/models/score.dart
+++ lib/models/score.dart
@@ -2,9 +2,11 @@ import 'package:flutter/material.dart';
 
 class Score with ChangeNotifier {
   int _count = 0;
+  int _count2 = 0;
  
   int get count => _count;
- 
+  int get count2 => _count2;
+  
   void increment() {
     _count++;
     notifyListeners();
