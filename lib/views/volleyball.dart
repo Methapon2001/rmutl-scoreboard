0563f6a376161b511c39ad0a34c479e6cb495306
Phumipat2000 <gaymanXIII@gmail.com>
2023-01-04 13:47:59 +0700

    Change Volleyball to VolleyballPage

--- lib/views/volleyball.dart
+++ lib/views/volleyball.dart
@@ -1,27 +1,26 @@
 import "package:flutter/material.dart";
 import 'package:scoreboard/widgets/menu.dart';
 
-class Volleyball extends StatefulWidget {
-  const Volleyball({super.key});
+class VolleyballPage extends StatefulWidget {
+  const VolleyballPage({super.key});
 
   @override
-  State<Volleyball> createState() => _VolleyballState();
+  State<VolleyballPage> createState() => _VolleyballPageState();
 }
 
-class _VolleyballState extends State<Volleyball> {
+class _VolleyballPageState extends State<VolleyballPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Volleyball"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [const Text('Volleyball')],
+        appBar: AppBar(
+          title: const Text("Volleyball"),
         ),
-      ),
-      endDrawer: const MenuDrawer(),
-    );
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [const Text('Volleyball')],
+          ),
+        ),
+        endDrawer: const MenuDrawer());
   }
 }
