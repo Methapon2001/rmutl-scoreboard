99c9f71f63bfbfde7db70008a1dad15c22406824
Phumipat2000 <gaymanXIII@gmail.com>
2023-01-06 16:22:29 +0700

    use an image as an icon

---
+++ lib/icon/icon.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MyIocns extends StatelessWidget {
+  const MyIocns({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset('image/test.png');
+  }
+}
