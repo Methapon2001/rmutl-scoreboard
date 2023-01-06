b4de0d89186cfd807741b28eebe583fced64e3dd
thanapat <koop0408@gmail.com>
2023-01-06 13:56:11 +0700

    adjustment

--- lib/widgets/button_quarter.dart
+++ lib/widgets/button_quarter.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:scoreboard/models/basketball/quarter.dart';
 import 'package:scoreboard/models/volleyball/quarter.dart';
+import 'package:scoreboard/models/soccer/quarter.dart';
 import 'package:provider/provider.dart';
 
 class ButtonQuarter extends StatefulWidget {
@@ -15,22 +16,41 @@ class ButtonQuarter extends StatefulWidget {
 class _ButtonQuarterState extends State<ButtonQuarter> {
   @override
   Widget build(BuildContext context) {
-    return TextButton(
-        onPressed: () {
-          if (widget.sport == 1) {
-            context.read<QuarterBasketball>().update();
-          }
-          if (widget.sport == 2){
-            context.read<QuarterVolleyball>().update();
-          }
-        },
-        child: widget.sport == 1 ? Text(
-          '${context.watch<QuarterBasketball>().getQuarter}',
-          style: const TextStyle(fontSize: 30),
-        ) : widget.sport == 2 ? Text(
-          '${context.watch<QuarterVolleyball>().getQuarter}',
-          style: const TextStyle(fontSize: 30),
-        ) : widget
-        );
+    return Column(
+      children: [
+        const Text(
+          'Quarter',
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+        TextButton(
+            onPressed: () {
+              if (widget.sport == 1) {
+                context.read<QuarterBasketball>().update();
+              }
+              if (widget.sport == 2) {
+                context.read<QuarterVolleyball>().update();
+              }
+              if (widget.sport == 3) {
+                context.read<QuarterSoccer>().update();
+              }
+            },
+            child: widget.sport == 1
+                ? Text(
+                    '${context.watch<QuarterBasketball>().getQuarter}',
+                    style: const TextStyle(fontSize: 30),
+                  )
+                : widget.sport == 2
+                    ? Text(
+                        '${context.watch<QuarterVolleyball>().getQuarter}',
+                        style: const TextStyle(fontSize: 30),
+                      )
+                    : widget.sport == 3
+                        ? Text(
+                            '${context.watch<QuarterSoccer>().getQuarter}',
+                            style: const TextStyle(fontSize: 30),
+                          )
+                        : widget)
+      ],
+    );
   }
 }
