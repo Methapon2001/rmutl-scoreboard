8ecfa1b5608b2e960ce1455b9519ddb55f4141d6
thanapat <koop0408@gmail.com>
2023-01-25 09:59:05 +0700

    resize icon

--- lib/icon/icon.dart
+++ lib/icon/icon.dart
@@ -18,41 +18,33 @@ class MyIcons extends StatelessWidget {
     List image = [];
     image1 = Image.asset(
       "image/home.png",
-      scale: 9.5,
+      scale: 11,
     );
     image2 = Image.asset(
       "image/basketball.png",
-      scale: 9,
+      scale: 11,
     );
     image3 = Image.asset(
       "image/volleyball.png",
-      scale: 10,
+      scale: 11,
     );
     image4 = Image.asset(
       "image/soccer.png",
-      scale: 10,
+      scale: 11,
     );
     image5 = Image.asset(
       "image/futsal.png",
-      scale: 10,
+      scale: 11,
     );
     image6 = Image.asset(
       "image/badminton.png",
-      scale: 10.5,
+      scale: 11,
     );
     image7 = Image.asset(
       "image/pingpong.png",
-      scale: 10,
+      scale: 11,
     );
     image = [image1, image2, image3, image4, image5, image6, image7];
-    return image[
-      sport == 1  ? 0
-        : sport == 2 ? 1
-            : sport == 3 ? 2
-                : sport == 4 ? 3
-                    : sport == 5 ? 4
-                        : sport == 6 ? 5
-                            : sport == 7 ? 6
-                            : 0];
+    return image[sport-1];
   }
 }
