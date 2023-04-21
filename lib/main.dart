7b71fb9cb03fa258dd9487af00a0ef15b464941e
Phumipat2000 <gaymanXIII@gmail.com>
2023-04-21 16:38:08 +0700

    add opps_screen

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,7 @@ import 'package:scoreboard/models/badminton/quarter.dart';
 import 'package:scoreboard/models/tabletennis/score.dart';
 import 'package:scoreboard/models/tabletennis/set.dart';
 import 'package:scoreboard/models/tabletennis/quarter.dart';
+// import 'package:scoreboard/opps/opps_screen.dart';
 
 void main() {
   runApp(const MyApp());
