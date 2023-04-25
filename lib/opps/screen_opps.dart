4a3cc21b819411e669d13b3e22db4306315f27a1
Phumipat2000 <gaymanXIII@gmail.com>
2023-04-25 12:08:47 +0700

    screen opps

---
+++ lib/opps/screen_opps.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:scoreboard/opps/opps.dart';
+// import 'package:flutter/src/widgets/container.dart';
+// import 'package:flutter/src/widgets/framework.dart';
+
+class Opps extends StatelessWidget {
+  const Opps({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: const OppsScreen(),
+    );
+  }
+}
