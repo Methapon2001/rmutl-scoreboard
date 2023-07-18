36f2db99fd3e07e9fcc7ed9c5fa6321a76acfd14
thanapat <koop0408@gmail.com>
2023-07-18 14:45:37 +0700

    create new page for setting

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,6 +1,6 @@
 import "package:flutter/material.dart";
+import 'package:scoreboard/widgets/image_sport.dart';
 import 'package:scoreboard/widgets/menu.dart';
-import 'package:scoreboard/widgets/connect_ip.dart';
 
 import '../widgets/bar.dart';
 
@@ -15,41 +15,15 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: Titlebar().appBar("Home"),
-      drawer: const MenuDrawer(index: 0),
-      body: Container(
-        color: const Color.fromARGB(255, 255, 242, 218),
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
-        child: Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        // ignore: prefer_const_literals_to_create_immutables
-        children: <Widget>[
-          SizedBox(
-            child: Container(
-              decoration: BoxDecoration(
-                  border: Border.all(
-                      color: const Color.fromARGB(255, 23, 36, 113),
-                      width: 8.0),
-                  borderRadius: const BorderRadius.all(
-                    Radius.circular(40),
-                  ),
-                  color: Colors.white,
-                  boxShadow: const [
-                    BoxShadow(
-                        color: Colors.black,
-                        blurRadius: 5,
-                        offset: Offset(4, 19))
-                  ]),
-              child: const ConnectIP(),
-            ),
-          ),
-        ],
-          ),
-        ),
-      ),
-    );
+        appBar: Titlebar().appBar("H O M E"),
+        drawer: const MenuDrawer(index: 0),
+        backgroundColor: MyBackgroundColor,
+        body: SingleChildScrollView(
+          physics: const NeverScrollableScrollPhysics(),
+          child: SizedBox(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              child: const ImageSports()),
+        ));
   }
 }
